a036a27a9974983101ec296dffe19a7faa1613bc
Nicole Bohra <nicolebohra@Nicoles-MacBook-Pro-2.local>
2019-04-06 22:24:00 -0700

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Organization } from './Organization';
+import { Jumbotron } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Jumbotron id = "title">The Home Project</Jumbotron>
+        <React.Fragment>
+          <style>{'body {background-color: #fafaed;}'}</style>
+          <div className="container">
+            <Link to="/organization">Organization?</Link>
+            {' '}
+            <Route path="/organization" component={ Organization } />
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
-
 export default App;
---
+++ src/Organization.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export class Organization extends Component{
+    render(){
+        return(<div>hello</div>)
+    }
+}
