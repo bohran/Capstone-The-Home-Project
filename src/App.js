caea6679534886787c8246134ec9176a2322c7c5
Nicole Bohra <nicolebohra@D-10-19-86-5.dhcp4.washington.edu>
2019-04-09 10:35:44 -0700

    imported font

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { Organization } from './Organization';
 import { Jumbotron } from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
@@ -11,9 +12,14 @@ class App extends Component {
         <Jumbotron id = "title">The Home Project</Jumbotron>
         <React.Fragment>
           <style>{'body {background-color: #fafaed;}'}</style>
+          <style>
+          @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,600');}
+          </style>
           <div className="container">
-            <Link to="/organization">Organization?</Link>
+          <button id = "orgButton">
+            <Link style={{textDecoration:'none'}} to="/organization">Organization?</Link>
             {' '}
+          </button>
             <Route path="/organization" component={ Organization } />
           </div>
         </React.Fragment>
