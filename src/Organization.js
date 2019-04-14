755c558860637bb3866fcf2fe4e5d2787dd8c9a7
Nicole Bohra <nicolebohra@Nicoles-MacBook-Pro-2.local>
2019-04-13 19:14:41 -0700

    FUK YA NAVBAR

--- src/Organization.js
+++ src/Organization.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { HashRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-export class Organization extends Component{
-    render(){
-        return(<div>hello</div>)
+
+export class Organization extends Component {
+    render() {
+        return (<div>Organization</div>)
     }
 }
+
