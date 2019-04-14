0dcced828a951bf0e41d4def549f50fdfcf9ab9d
shannongatta1 <shannon.gatta@gmail.com>
2019-04-13 20:09:06 -0700

    page names changed

---
+++ src/Submission.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Organization } from './Organization';
+import { Jumbotron } from 'reactstrap';
+
+import './App.css';
+
+class Submission extends Component {
+  render() {
+    return (
+        <div class="formContainer">
+        <div>
+        <h1 >Your event has been submitted for approval!</h1>
+        <p>We've sent your event to Pyramid for approval. The event should be approved in the next business day. 
+            Please contact us at eropes@uw.edu to make changes to your event posting or 
+            any other inquiries related to your event.</p>
+        </div>
+        <div class="buttonContainer">
+            <button class="submitButton" ><a class="eventLink" href="../index.html"> Return to Calendar </a> </button>
+            <button class="submitButton"><a class="eventLink" href="./events.html"> Submit Another Event</a> </button>
+            </div>
+        </div>
+    );
+  }
+}
+export default Submission;
