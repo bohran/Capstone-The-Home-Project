32a4917559ac5c3ad7bc2b91c083bdbb4848b071
Nicole Bohra <nicolebohra@Nicoles-MacBook-Pro-2.local>
2019-04-13 21:15:20 -0700

    addEvent route added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Home } from './Home';
 import { Events } from './Events';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Jumbotron } from 'reactstrap';
 
-import './App.css';
+import './css/App.css'
 
 class App extends Component {
   render() {
@@ -14,14 +14,15 @@ class App extends Component {
         <div>
           <Navbar className='nav-wrapper' dark expand="lg"style={{outerHeight:"100px"}}>
                 <NavbarBrand className = "title" style={{color:"black", fontSize:"50px", paddingRight:"250px", paddingLeft:"25px"}}href = "/">The Home Project</NavbarBrand>
-                 <Link className = "links" to="/Events" style={{fontSize:"25px", paddingLeft:"200px"}}>Events</Link>
-                 <Link className = "links" to="/About" style={{fontSize:"25px",paddingLeft:"40px"}}>About Us</Link>
-                 <Link className = "links" to="/Contact" style={{fontSize:"25px",paddingLeft:"40px"}}>Contact Us</Link>   
+                 <Link className = "links" to="/Events" style={{paddingLeft:"200px"}}>Events</Link>
+                 <Link className = "links" to="/About">About Us</Link>
+                 <Link className = "links" to="/Contact">Contact Us</Link>   
           </Navbar>
           <Switch>
             <Route exact path = "/" component = { Home } />
             <Route path="/AddOrganization" component = { Organization } />
             <Route path = "/Events" component = { Events } />
+             <Route path = "/AddEvent" component = { AddEvent } />
             {/* <Route path = "Organization/Confirmation" component = { Confirmation } />
             <Route path = "Organization/Confirmation/Submission" component = { Review } /> */}
           </Switch>
