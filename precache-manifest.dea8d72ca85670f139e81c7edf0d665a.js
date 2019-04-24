67135c3e8c23a8b3842e65177923f1643bcd296f
mteshmore <moree@uw.edu>
2019-04-23 17:16:40 -0700

    Updates

---
+++ precache-manifest.dea8d72ca85670f139e81c7edf0d665a.js
@@ -0,0 +1,242 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5d6a4c06af7e528fab514cabe76ed17",
+    "url": "/capstone-home-project-sp19/static/media/tent.d5d6a4c0.jpg"
+  },
+  {
+    "revision": "cf25b1c93d5fe8454111",
+    "url": "/capstone-home-project-sp19/static/css/main.0abfd36c.chunk.css"
+  },
+  {
+    "revision": "f120baba5f90daaef4e7",
+    "url": "/capstone-home-project-sp19/static/js/runtime~main.5ac44a5c.js"
+  },
+  {
+    "revision": "708845e875a3b08640369bf84fd65472",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-900italic.708845e8.woff"
+  },
+  {
+    "revision": "e5defd52587ed42ad5b1",
+    "url": "/capstone-home-project-sp19/static/js/2.380326ad.chunk.js"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/capstone-home-project-sp19/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/capstone-home-project-sp19/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/capstone-home-project-sp19/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "6c3877898e39f3cf9044e80df76e7c0d",
+    "url": "/capstone-home-project-sp19/static/media/NunitoSans-ExtraBold.6c387789.ttf"
+  },
+  {
+    "revision": "edcff4728f956d5194a03b76a158c834",
+    "url": "/capstone-home-project-sp19/static/media/NunitoSans-Bold.edcff472.ttf"
+  },
+  {
+    "revision": "b35ea69737d6300e7a721e628b9e239a",
+    "url": "/capstone-home-project-sp19/static/media/Seattle-WA.b35ea697.jpg"
+  },
+  {
+    "revision": "cf25b1c93d5fe8454111",
+    "url": "/capstone-home-project-sp19/static/js/main.569e4c28.chunk.js"
+  },
+  {
+    "revision": "0aa9029aa5c94ee0245428787ff2aa8b",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-200.0aa9029a.woff"
+  },
+  {
+    "revision": "3f69f8c78eb965be709fada6e9326097",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-200.3f69f8c7.woff2"
+  },
+  {
+    "revision": "da7c09610c91ac990644a19f2d713357",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-200italic.da7c0961.woff"
+  },
+  {
+    "revision": "6efd102c0b004908b9454b4e4610d693",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-200italic.6efd102c.woff2"
+  },
+  {
+    "revision": "f16977f35222a4cce7d6f4336732a67a",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-300.f16977f3.woff2"
+  },
+  {
+    "revision": "e7a891b0ad0ce9050b19e3d0cdfcd860",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-300.e7a891b0.woff"
+  },
+  {
+    "revision": "246f6e2b70f0b23e366947c975effa65",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-300italic.246f6e2b.woff2"
+  },
+  {
+    "revision": "1a3d2422361712bf569edf8b2e1f6d90",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-300italic.1a3d2422.woff"
+  },
+  {
+    "revision": "9c6a1ff29aaba0c21496eac04be062df",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-400.9c6a1ff2.woff2"
+  },
+  {
+    "revision": "8ace450709844437a579d4f4c7c37b42",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-400.8ace4507.woff"
+  },
+  {
+    "revision": "e14ffefb3a60e5d5a9a47519882f92e8",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-400italic.e14ffefb.woff"
+  },
+  {
+    "revision": "d44697ce7f31cee44f701572fb38cab5",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-400italic.d44697ce.woff2"
+  },
+  {
+    "revision": "5a71131c00b436beb97dd2c570768f05",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-600.5a71131c.woff"
+  },
+  {
+    "revision": "b56c7e1dd3d7a5fad61e46f4dd553294",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-600.b56c7e1d.woff2"
+  },
+  {
+    "revision": "0de4a8bace81fc8b1f570af06a28e385",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-600italic.0de4a8ba.woff2"
+  },
+  {
+    "revision": "9db53e390a5327462f45889e249f84d6",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-600italic.9db53e39.woff"
+  },
+  {
+    "revision": "d0b6d0f05c00552a21519d6bdb3e5ed7",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-700.d0b6d0f0.woff2"
+  },
+  {
+    "revision": "1aa5d63eca4940fbbb5181895e9bc008",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-700.1aa5d63e.woff"
+  },
+  {
+    "revision": "c7569232f38ac2b1b3211151127b1ed3",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-700italic.c7569232.woff2"
+  },
+  {
+    "revision": "ffc1f5c22dc4b08f710c027976972de1",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-700italic.ffc1f5c2.woff"
+  },
+  {
+    "revision": "a28845a6196298dad6dc0b2ba0b6de55",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-800.a28845a6.woff"
+  },
+  {
+    "revision": "6a4c29ad18bd8072a9a9d2443d0e340e",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-800.6a4c29ad.woff2"
+  },
+  {
+    "revision": "0117d6315806b392f7da0d5d3aa9c7ca",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-800italic.0117d631.woff2"
+  },
+  {
+    "revision": "4f4e7a921cc52e9458245f66eb550004",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-800italic.4f4e7a92.woff"
+  },
+  {
+    "revision": "e22a661eeed6baa960d645c48de5a00f",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-900.e22a661e.woff2"
+  },
+  {
+    "revision": "412c6f01c1eb3a9135d53bc4fd220a35",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-900.412c6f01.woff"
+  },
+  {
+    "revision": "0dc1055cf601fe66514482613ebdf168",
+    "url": "/capstone-home-project-sp19/static/media/nunito-sans-latin-900italic.0dc1055c.woff2"
+  },
+  {
+    "revision": "e5defd52587ed42ad5b1",
+    "url": "/capstone-home-project-sp19/static/css/2.4f8dfb70.chunk.css"
+  },
+  {
+    "revision": "76f2f7138b81767d3a904b6cfcf79879",
+    "url": "/capstone-home-project-sp19/index.html"
+  }
+];
--- winter19_code/clients/js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-$('.where a').click(function(){
-    $('#city-selected').text($(this).text());
-  });
-
-  $('.when a').click(function(){
-    $('#date-selected').text($(this).text());
-  });
